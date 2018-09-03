b7e12e04a7749f9b53d648bd49433560989f9687
Jefferson Oliveira <luizajeff19@gmail.com>
2018-09-02 22:17:35 -0300

    criamos a classes page

---
+++ views-cache/header.b9fe4adb4296722f88cb7272732a5535.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>title</title>
+</head>
+<body>
