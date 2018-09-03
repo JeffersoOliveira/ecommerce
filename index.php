ec65893ff825c161be8a81203887b1047a699ff5
jefferson <luizajeff19@gmail.com>
2018-09-03 19:54:38 -0300

    tlp admin

--- index.php
+++ index.php
@@ -4,11 +4,20 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use  \Hcode\PageAdmin;
 
 $app = new Slim();
 
 
 $app->config('debug', true);
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+    $page->setTpl("index");
+
+
+});
 
 $app->get('/', function() {
 
